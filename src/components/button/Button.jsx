c99595614081938f6351f33cee1d736d9588a929
Daniel Luan Lourenço <danielluanlourencol@gmail.com>
2023-03-05 18:54:09 -0300

    first commit

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import "./Button.css"
+
+const Button = props => {
+    return (
+        <button
+            onClick={e => props.click(props.label)}
+            className={`
+            button
+            ${props.operation ? 'operation' : ''}
+            ${props.double ? 'double' : ''}
+            ${props.triple ? 'triple' : ''}
+        `}>
+            {props.label}
+        </button>
+    )
+}
+
+export default Button
